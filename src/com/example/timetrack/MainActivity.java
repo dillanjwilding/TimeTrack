514784113134761a1aca7b2c4f66ee01dab9a4bb
Dillan Wilding <DillanJWilding@gmail.com>
2013-02-15 12:22:11 -0500

    Fixing merge issues

--- src/com/example/timetrack/MainActivity.java
+++ src/com/example/timetrack/MainActivity.java
@@ -2,31 +2,12 @@ package com.example.timetrack;
 
 import android.os.Bundle;
 import android.app.Activity;
-<<<<<<< HEAD
-import android.view.Menu;
-
-public class MainActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
-=======
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends Activity {
-	
+
 	// Called at the start of the fill lifetime.
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,5 +32,4 @@ public class MainActivity extends Activity {
 		startActivity(i);
 		finish();
 	}
->>>>>>> 1a619541cd51884bce0d5b78c410bbe61aa795fb
 }
